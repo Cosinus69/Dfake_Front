962d66b289ba9c663b31235a19b7fa8ab22ef206
Hafida ATID <hafidatid@orange.fr>
2026-03-11 13:14:24 +0100

    App V2 ready:with API URL predict_image

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import random
 
 
 #CONSTANT
-#API_URL = 'http://localhost:8000/predict_image/'
-
+API_URL='http://127.0.0.1:8000/predict_image/'
+API_URL2= 'http://127.0.0.1:8000/generate_heatmap/'
 
 #-------------------------
 # Configuration des pages
@@ -27,7 +27,7 @@ st.title("D-FAKE App - Frontend Demo")
 def validate_image(uploaded_file, max_size=4000): #Taille à valider avec équipe
     try:
         image = Image.open(uploaded_file)
-        image.verify()  # vérification de l' intégrité
+        image.verify()  # vérification de l'intégrité
     except Exception:
         st.error("Corrupted image file")
         return None
@@ -52,26 +52,6 @@ def validate_image(uploaded_file, max_size=4000): #Taille à valider avec équip
 
     return image
 
-# --------------------------------------------------------------------
-# Simulation prediction en attendant la fonction "predict_image" (Thomas)
-# --------------------------------------------------------------------
-def mock_predict(image):
-    labels = ["Fake", "Real"]
-    label = random.choice(labels)
-    confidence = random.uniform(0.5, 1.0)
-    return label, confidence
-
-# ---------------------------------------------------------------------
-# Simulation Grad-CAM en attendant la fonction "generate heatmap" (Diego)
-# ---------------------------------------------------------------------
-def mock_gradcam(image):
-    h, w = image.height, image.width
-    heatmap = np.random.rand(h, w)
-    heatmap = cv2.applyColorMap(np.uint8(255*heatmap), cv2.COLORMAP_JET)
-    img = np.array(image)
-    superimposed_img = cv2.addWeighted(img, 0.6, heatmap, 0.4, 0)
-    return Image.fromarray(superimposed_img)
-
 
 # -------------------------
 # Sidebar Menu
@@ -82,18 +62,18 @@ menu = st.sidebar.selectbox(
     ["Home","Check a picture"]
 )
 
-# Home
-#-------
+# Home (dans menu déroulant)
+#----------------------------
 if menu == "Home":      # Dans page home on pourra faire un descriptif
-                        # Je ferai des propositions
+                        # Je ferai des propositions avec logo wagon, Batch 2225..
 
     st.header("D-Fake Project")
 
     st.write("Upload an image to check if it's a Fake or Real.")
     st.write("This is a frontend demo: predictions and Grad-CAM are simulated.")
 
-# Check a picture
-#------------------
+# Check a picture (dans menu déroulant)
+#---------------------------------------
 elif menu == "Check a picture":
 
     st.header("Image Detection")
@@ -102,33 +82,35 @@ elif menu == "Check a picture":
         "Upload your image",
         type=["jpg","jpeg","png","gif","bmp","webp"] # filtrage au niveau de l'interface
 )
-
-
+    # Un fichier a été uploadé
     if uploaded_file is not None:  # evite à Streamlit de bugger
-         # validation préalable de l'image loadée
+         # validation préalable de l'image loadée et conversion en RGB
          image = validate_image(uploaded_file) # return image ou messages d'erreur à l'utilisateur si l'image n'est pas validée
          if image is not None :
-             #on affiche l'image originale
+            st.image(image, caption="Uploaded Picture", use_container_width=True)
+            # Remise du pointeur au début après image.verify sinon fichier vide et erreur connection API
+            uploaded_file.seek(0)
+            #Envoi du fichier au back-end API
+            files = {"file" : uploaded_file }
+            response = requests.post(API_URL, files=files)
+         if response.status_code == 200:
+             data  = response.json()
+             label = data["fake_real"]
+             confidence = data["predict_value"]
 
-             #st.image(image, caption="Uploaded Picture", use_container_width=True)
-
-             # simulation de la prediction en attendant la fonction
-             label, confidence = mock_predict(image)
              #st.write(f"Your picture is a **{label}** one !")
              # Real en vert et Fake en rouge
              if label == "Fake":
-                st.markdown(f'<p style="color:red; font-weight:bold;">Your picture is a {label} one !</p>', unsafe_allow_html=True)
+                    st.markdown(f'<p style="color:red; font-weight:bold;">Your picture is a {label} one !</p>', unsafe_allow_html=True)
              else:
-                st.markdown(f'<p style="color:green; font-weight:bold;">Prediction: {label}</p>', unsafe_allow_html=True)
+                    st.markdown(f'<p style="color:green; font-weight:bold;">Your picture is a {label} one !</p>', unsafe_allow_html=True)
 
              # Barre de confiance
              st.write(f"Confidence level: **{confidence*100:.2f}%**") # on met les probas en pourcentage
              st.progress(int(confidence*100))
-             #st.metric(label="Confidence", value=f"{confidence*100:.2f}%", delta_color="inverse")
 
-             #simulation Grad CAM en attendant la fonction, prévoir que si image est fake !
-             cam_image = mock_gradcam(image)
-             #st.image(cam_image, caption=f"{label} - Importantes Zones", use_container_width=True)
-             col1, col2 = st.columns(2)
-             col1.image(image, caption="Uploaded Image", use_container_width=True)
-             col2.image(cam_image, caption=f"{label} - Important Zones", use_container_width=True)
+         else:
+                 st.error("Error calling the API")
+
+    else:
+        st.write("No file uploaded")
