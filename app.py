140081bf5a1a037da311c4ac89391bcbf9a64a35
Hafida ATID <hafidatid@orange.fr>
2026-03-11 16:45:09 +0100

    App V3 ready :with API URL generate_heatmap + update requirements file

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import streamlit as st
-from PIL import Image
-import numpy as np
+from PIL import Image #Package pillow =PIL
+
+
+# Déjà installés dans le package Python version 3.12.9
 import requests
 import cv2
 import random
+import base64
+import io
 
 
 #CONSTANT
+
+#API url
 API_URL='http://127.0.0.1:8000/predict_image/'
 API_URL2= 'http://127.0.0.1:8000/generate_heatmap/'
 
@@ -87,20 +93,23 @@ elif menu == "Check a picture":
          # validation préalable de l'image loadée et conversion en RGB
          image = validate_image(uploaded_file) # return image ou messages d'erreur à l'utilisateur si l'image n'est pas validée
          if image is not None :
-            st.image(image, caption="Uploaded Picture", use_container_width=True)
-            # Remise du pointeur au début après image.verify sinon fichier vide et erreur connection API
+            #st.image(image, caption="Uploaded Picture", use_container_width=True)
+            #Remise du pointeur au début après image.verify sinon fichier vide et erreur connection API
             uploaded_file.seek(0)
             #Envoi du fichier au back-end API
             files = {"file" : uploaded_file }
-            response = requests.post(API_URL, files=files)
+            response = requests.post(API_URL2, files=files)
+
          if response.status_code == 200:
              data  = response.json()
              label = data["fake_real"]
              confidence = data["predict_value"]
+             image_uploaded_data = base64.b64decode(data["image_resized"]) # Image est en binaire
+             image_heatmap_data = base64.b64decode(data["heatmap"]) # Heatmap est en binaire
 
              #st.write(f"Your picture is a **{label}** one !")
              # Real en vert et Fake en rouge
-             if label == "Fake":
+             if label == "FAKE":
                     st.markdown(f'<p style="color:red; font-weight:bold;">Your picture is a {label} one !</p>', unsafe_allow_html=True)
              else:
                     st.markdown(f'<p style="color:green; font-weight:bold;">Your picture is a {label} one !</p>', unsafe_allow_html=True)
@@ -108,6 +117,21 @@ elif menu == "Check a picture":
              # Barre de confiance
              st.write(f"Confidence level: **{confidence*100:.2f}%**") # on met les probas en pourcentage
              st.progress(int(confidence*100))
+
+             # Transformation des deux images binaires en fichier mémoire ouvrables avec PIL
+             initial_image_uploaded = Image.open(io.BytesIO(image_uploaded_data))
+             image_heatmap = Image.open(io.BytesIO(image_heatmap_data))
+
+             # Option 1 : Affichage images côte à côte
+             #----------
+             col1, col2 = st.columns(2)
+             col1.image(initial_image_uploaded , caption="Uploaded Image", use_container_width=True)
+             col2.image(image_heatmap , caption=f"{label} - Important Zones", use_container_width=True)
+
+             #Option 2 : affichage images l'une en dessous de l'autre(Visuel moins écrasé)
+             #---------
+             #st.image(initial_image_uploaded , caption="Uploaded Image", use_container_width=True)
+             #st.image(image_heatmap , caption=f"{label} - Important Zones", use_container_width=True)
 
          else:
                  st.error("Error calling the API")
